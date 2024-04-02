2d090e8912ff9a0be82464ac0d18e1ce6298651e
Ankur Gupta <ankurkumargupta12345678@gmail.com>
2024-04-02 14:58:23 +0530

    Commited first

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -20,7 +20,7 @@ router.post("/addnote", fetchuser, [
     body("title", "Enter valid title").isLength({ min: 3 }),
     body("description", "Enter valid description").isLength({ min: 5 }),
 ], async (req, res) => {
-    // console.log(req.body);
+    // console.log(req.body)
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
